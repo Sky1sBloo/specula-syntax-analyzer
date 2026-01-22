54685a2f91f341608843b1ee7da996997e2034c7
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-22 08:31:28 +0800

    Made exception output a message

--- SpeculaSyntaxAnalyzer/LexerReader/LexerReadException.cs
+++ SpeculaSyntaxAnalyzer/LexerReader/LexerReadException.cs
@@ -4,9 +4,11 @@ namespace SpeculaSyntaxAnalyzer.LexerReader;
 
 public class LexerReadException : ArgumentException
 {
+    public override string Message { get; }
     public LexerReadException(Token token, string message)
     {
         StringBuilder outMsg = new StringBuilder();
         outMsg.AppendFormat("Token: {0} at {1}:{2}. {3}", token.Type.ToString(), token.Line, token.CharStart, message);
+        Message = outMsg.ToString();
     }
 }
