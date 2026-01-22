159ece09cf2ca55f241be3a2b88c67a880155752
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-22 18:13:57 +0800

    Made value handler have logic for returning values

--- SpeculaSyntaxAnalyzer/SyntaxAnalyzer/Handlers/Handler.cs
+++ SpeculaSyntaxAnalyzer/SyntaxAnalyzer/Handlers/Handler.cs
@@ -6,7 +6,7 @@ public abstract class Handler
     public event Action<ParseNode>? Finished;
     public event Action<SyntaxAnalyzerRoot.States>? DelegateToState;
 
-    public abstract void handleToken(Token token);
+    public abstract void handleToken(Token token, ParseNode? prevNode);
     protected void SetHandlerFinished(ParseNode node) => Finished?.Invoke(node);
     protected void SetDelegateToState(SyntaxAnalyzerRoot.States state) => DelegateToState?.Invoke(state);
 }
