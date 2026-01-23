9a89b45dc6167f9eb1bc123ce9254e8db2c8ee8d
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-23 20:26:56 +0800

    Added for loop handler

--- SpeculaSyntaxAnalyzer/ParseTree/AssignNode.cs
+++ SpeculaSyntaxAnalyzer/ParseTree/AssignNode.cs
@@ -1,10 +1,10 @@
 namespace SpeculaSyntaxAnalyzer.ParseTree;
 
-public interface Assignment : Statement;
-public record AssignmentStatementNode(string Identifier, Expression value) : Assignment, ForInit;
-public record AssignPlusEqNode(string Identifier, Expression value) : Assignment, ForInit;
-public record AssignMinusEqNode(string Identifier, Expression value) : Assignment, ForInit;
-public record AssignMulEqNode(string Identifier, Expression value) : Assignment, ForInit;
-public record AssignDivEqNode(string Identifier, Expression value) : Assignment, ForInit;
-public record AssignModEqNode(string Identifier, Expression value) : Assignment, ForInit;
+public interface Assignment : Statement, ForInit;
+public record AssignmentStatementNode(string Identifier, Expression value) : Assignment;
+public record AssignPlusEqNode(string Identifier, Expression value) : Assignment;
+public record AssignMinusEqNode(string Identifier, Expression value) : Assignment;
+public record AssignMulEqNode(string Identifier, Expression value) : Assignment;
+public record AssignDivEqNode(string Identifier, Expression value) : Assignment;
+public record AssignModEqNode(string Identifier, Expression value) : Assignment;
 
