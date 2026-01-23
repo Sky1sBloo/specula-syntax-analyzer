f15cfe73af162806ccfd8461155d8bb6755abc0b
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-23 19:43:54 +0800

    Fixed issue with body handler throwing on incomplete statements

--- SpeculaSyntaxAnalyzer/SyntaxAnalyzer/Handlers/BodyHandler.cs
+++ SpeculaSyntaxAnalyzer/SyntaxAnalyzer/Handlers/BodyHandler.cs
@@ -14,13 +14,32 @@ public class BodyHandler : Handler
 
     protected override ParseNode? verifyTokens()
     {
+        // reset statements for each new body parse to avoid leaking previous state
+        statements = new();
         if (CurrentToken.Type != Token.Types.D_CBRAC_OP)
         {
             throw new SyntaxErrorException(["{"], CurrentToken);
         }
         incrementIndex();
-        while (CurrentToken.Type != Token.Types.D_CBRAC_CLO)
+        while (true)
         {
+            if (!HasMoreTokens)
+            {
+                Token missing = new()
+                {
+                    Type = Token.Types.UNKNOWN,
+                    Line = getIndex(),
+                    CharStart = 0,
+                    CharEnd = 0
+                };
+                throw new SyntaxErrorException(["}"], missing);
+            }
+
+            if (CurrentToken.Type == Token.Types.D_CBRAC_CLO)
+            {
+                break;
+            }
+
             switch (CurrentToken.Type)
             {
                 case Token.Types.K_LET:
@@ -42,6 +61,7 @@ public class BodyHandler : Handler
             /// call func
             /// spawn thread
 
+            incrementIndex();
         }
         return new BodyNode(statements);
     }
