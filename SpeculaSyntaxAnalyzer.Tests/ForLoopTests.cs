c0f67e9fa8544837db44a1f972a074d5a60d0d11
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-23 21:58:10 +0800

    Added tests

---
+++ SpeculaSyntaxAnalyzer.Tests/ForLoopTests.cs
@@ -0,0 +1,81 @@
+using SpeculaSyntaxAnalyzer.SyntaxAnalyzer;
+using SpeculaSyntaxAnalyzer.LexerReader;
+using SpeculaSyntaxAnalyzer.ParseTree;
+
+namespace SpeculaSyntaxAnalyzer.Tests;
+
+[TestFixture]
+public class ForLoopTests
+{
+    private ForLoopHandler forHandler = null!;
+    private ErrorsHandler errorsHandler = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        errorsHandler = new();
+        forHandler = new(errorsHandler);
+    }
+
+    #region Valid Cases
+
+    [Test]
+    public void ForWithIncrement()
+    {
+        var output = LexerFileReader.ParseFile("Samples/For/ForIncrement.json");
+        HandlerOutput result = forHandler.HandleToken(output.Tokens, 0);
+        Assert.That(result.node, Is.InstanceOf<ForLoop>());
+        var forLoop = (ForLoop)result.node!;
+        Assert.That(forLoop.init, Is.InstanceOf<AssignmentStatementNode>());
+        Assert.That(forLoop.assignment, Is.InstanceOf<AssignmentStatementNode>());
+    }
+
+    [Test]
+    public void ForWithBody()
+    {
+        var output = LexerFileReader.ParseFile("Samples/For/ForWithBody.json");
+        HandlerOutput result = forHandler.HandleToken(output.Tokens, 0);
+        Assert.That(result.node, Is.InstanceOf<ForLoop>());
+    }
+
+    #endregion
+
+    #region Invalid Cases
+
+    [Test]
+    public void MissingForKeyword_ShouldThrow()
+    {
+        var output = LexerFileReader.ParseFile("Samples/For/Invalid/MissingForKeyword.json");
+        Assert.Throws<SyntaxErrorException>(() => forHandler.HandleToken(output.Tokens, 0));
+    }
+
+    [Test]
+    public void MissingOpenParenthesis_ShouldThrow()
+    {
+        var output = LexerFileReader.ParseFile("Samples/For/Invalid/MissingOpenParenthesis.json");
+        Assert.Throws<SyntaxErrorException>(() => forHandler.HandleToken(output.Tokens, 0));
+    }
+
+    [Test]
+    public void MissingCloseParenthesis_ShouldThrow()
+    {
+        var output = LexerFileReader.ParseFile("Samples/For/Invalid/MissingCloseParenthesis.json");
+        Assert.Throws<SyntaxErrorException>(() => forHandler.HandleToken(output.Tokens, 0));
+    }
+
+    [Test]
+    public void MissingFirstSemicolon_ShouldThrow()
+    {
+        var output = LexerFileReader.ParseFile("Samples/For/Invalid/MissingFirstSemicolon.json");
+        Assert.Throws<SyntaxErrorException>(() => forHandler.HandleToken(output.Tokens, 0));
+    }
+
+    [Test]
+    public void MissingSecondSemicolon_ShouldThrow()
+    {
+        var output = LexerFileReader.ParseFile("Samples/For/Invalid/MissingSecondSemicolon.json");
+        Assert.Throws<SyntaxErrorException>(() => forHandler.HandleToken(output.Tokens, 0));
+    }
+
+    #endregion
+}
