4d9752491cfa3f71a210dfe50597359d239ec44e
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-23 20:03:48 +0800

    Made assignment statements inherit assignment interface

--- SpeculaSyntaxAnalyzer/ParseTree/AssignNode.cs
+++ SpeculaSyntaxAnalyzer/ParseTree/AssignNode.cs
@@ -1,9 +1,10 @@
 namespace SpeculaSyntaxAnalyzer.ParseTree;
 
-public record AssignmentStatementNode(string Identifier, Expression value) : Statement, ForInit;
-public record AssignPlusEqNode(string Identifier, Expression value) : Statement, ForInit;
-public record AssignMinusEqNode(string Identifier, Expression value) : Statement, ForInit;
-public record AssignMulEqNode(string Identifier, Expression value) : Statement, ForInit;
-public record AssignDivEqNode(string Identifier, Expression value) : Statement, ForInit;
-public record AssignModEqNode(string Identifier, Expression value) : Statement, ForInit;
+public interface Assignment : Statement;
+public record AssignmentStatementNode(string Identifier, Expression value) : Assignment, ForInit;
+public record AssignPlusEqNode(string Identifier, Expression value) : Assignment, ForInit;
+public record AssignMinusEqNode(string Identifier, Expression value) : Assignment, ForInit;
+public record AssignMulEqNode(string Identifier, Expression value) : Assignment, ForInit;
+public record AssignDivEqNode(string Identifier, Expression value) : Assignment, ForInit;
+public record AssignModEqNode(string Identifier, Expression value) : Assignment, ForInit;
 
