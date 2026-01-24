99b300258d5c201fc76e19bcb47bb4906d835803
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-24 22:04:47 +0800

    Added conditional statement else ifelse

---
+++ SpeculaSyntaxAnalyzer/SyntaxAnalyzer/Handlers/ConditionalStatementHandler.cs
@@ -0,0 +1,103 @@
+using SpeculaSyntaxAnalyzer.ParseTree;
+
+namespace SpeculaSyntaxAnalyzer.SyntaxAnalyzer;
+
+/// <summary>
+/// Contains conditions 
+/// </summary>
+public class ConditionalStatementHandler : Handler
+{
+    private readonly ExpressionHandler expressionHandler;
+    private readonly BodyHandler bodyHandler;
+
+    public ConditionalStatementHandler(ErrorsHandler errors) : base(errors)
+    {
+        expressionHandler = new(errors);
+        bodyHandler = new(errors);
+    }
+
+    protected override ParseNode? verifyTokens()
+    {
+        if (CurrentToken.Type != Token.Types.K_IF)
+        {
+            throw new SyntaxErrorException(["if"], CurrentToken);
+        }
+        incrementIndex();
+        IfStatementNode ifStatement = ParseIfStatement();
+        PrintableList<ElseIfStatementNode> elseIfStatements = new();
+        ElseStatement? elseStatement = null;
+        while (true)
+        {
+            if (CurrentToken.Type == Token.Types.K_ELSE)
+            {
+                incrementIndex();
+                if (CurrentToken.Type == Token.Types.K_IF)
+                {
+                    incrementIndex();
+                    elseIfStatements.Add(parseElseIfStatement());
+                }
+                else
+                {
+                    elseStatement = parseElseStatement();
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        return new ConditionalStatement(ifStatement, elseIfStatements, elseStatement);
+    }
+
+    // already consumed the 'if' token
+    private IfStatementNode ParseIfStatement()
+    {
+        return (IfStatementNode)parseConditionBody(true);
+    }
+    // already consumed the 'else' and 'if' tokens
+    private ElseIfStatementNode parseElseIfStatement()
+    {
+        return (ElseIfStatementNode)parseConditionBody(false);
+    }
+    private IfNode parseConditionBody(bool buildIfStatement)
+    {
+
+        if (CurrentToken.Type != Token.Types.D_PAR_OP)
+        {
+            SyntaxErrorException ex = new SyntaxErrorException(["("], CurrentToken);
+            errorHandler.AddError(ex);
+            throw ex;
+        }
+
+        ParseNode? conditionNode = delegateToHandler(expressionHandler);
+        if (conditionNode is null)
+        {
+            throw new SyntaxErrorException(["Expression"], CurrentToken);
+        }
+        Expression conditionExpr = (Expression)conditionNode;
+
+        ParseNode? bodyNode = delegateToHandler(bodyHandler);
+        if (bodyNode is null)
+        {
+            throw new SyntaxErrorException(["Body"], CurrentToken);
+        }
+        BodyNode body = (BodyNode)bodyNode;
+        if (buildIfStatement)
+        {
+            return new IfStatementNode(conditionExpr, body);
+        }
+        else
+        {
+            return new ElseIfStatementNode(conditionExpr, body);
+        }
+    }
+    private ElseStatement parseElseStatement()
+    {
+        ParseNode? bodyNode = delegateToHandler(bodyHandler);
+        if (bodyNode is null)
+        {
+            throw new SyntaxErrorException(["Body"], CurrentToken);
+        }
+        return new ElseStatement((BodyNode)bodyNode);
+    }
+}
