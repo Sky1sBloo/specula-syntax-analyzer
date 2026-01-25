320c701256e844e8da8493064c142bb7c323a9d9
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-25 12:01:51 +0800

    Added imports handler

--- SpeculaSyntaxAnalyzer/ParseTree/ModuleNodes.cs
+++ SpeculaSyntaxAnalyzer/ParseTree/ModuleNodes.cs
@@ -3,6 +3,11 @@ namespace SpeculaSyntaxAnalyzer.ParseTree;
 
 public interface ModuleNode : RootStatement;
 
-public record ExportNode(Statement statement) : ModuleNode;
-public record ImportDefaultNode(string moduleName, string alias) : ModuleNode;
-public record ImportNode(string moduleName, PrintableList<string> identifiers) : ModuleNode;
+public interface ExportModuleNode : ModuleNode;
+public record ExportNode(Statement statement) : ExportModuleNode;
+public record ExportDefaultNode(Statement statement) : ExportModuleNode;
+
+public interface ImportModuleNode : ModuleNode;
+public record ImportNodes(PrintableList<ImportModuleNode> imports) : ModuleNode;
+public record ImportAliasNode(string moduleName, string alias) : ImportModuleNode;
+public record ImportNode(string moduleName, PrintableList<string> identifiers) : ImportModuleNode;
