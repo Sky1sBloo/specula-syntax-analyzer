56a9f52f243bcfd9b1d51addb85eaf49a523a842
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-25 22:08:28 +0800

    Added contract handler definition and placed it on syntax analyzer root

--- SpeculaSyntaxAnalyzer/ParseTree/Contracts/Contract.cs
+++ SpeculaSyntaxAnalyzer/ParseTree/Contracts/Contract.cs
@@ -6,4 +6,4 @@ public record ContractNode(string Identifier,
     PrintableList<StateTransitionsNode> StateTransitions,
     PrintableList<ContractMessageEventsNode> MessageEvents,
     PrintableList<ContractEventNode> Events
-    ) : ParseNode;
+    ) : RootStatement;
