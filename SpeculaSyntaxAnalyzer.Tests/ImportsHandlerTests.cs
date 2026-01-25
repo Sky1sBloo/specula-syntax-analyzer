f0c54247740fc63e951ef75f4131b09c3fb3bc4f
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-25 13:40:15 +0800

    Added import and export tests

---
+++ SpeculaSyntaxAnalyzer.Tests/ImportsHandlerTests.cs
@@ -0,0 +1,62 @@
+using SpeculaSyntaxAnalyzer.SyntaxAnalyzer;
+using SpeculaSyntaxAnalyzer.LexerReader;
+using SpeculaSyntaxAnalyzer.ParseTree;
+
+namespace SpeculaSyntaxAnalyzer.Tests;
+
+[TestFixture]
+public class ImportsHandlerTests
+{
+    private SyntaxAnalyzerRoot analyzer = new();
+
+    [SetUp]
+    public void Setup()
+    {
+        analyzer = new();
+    }
+
+    [Test]
+    public void ImportNamedExport()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Import/ImportNamedExport.json");
+        ParseNode? node = analyzer.ReadTokens(output.Tokens);
+        Assert.That(node, Is.Not.Null);
+        if (node is RootNode rootNode)
+        {
+            Assert.That(rootNode.statements.Count, Is.EqualTo(1));
+            var importNode = rootNode.statements[0];
+            Assert.That(importNode, Is.Not.Null);
+        }
+        Assert.That(analyzer.ErrorHandler.ErrorList.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ImportMultipleNamedExports()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Import/ImportMultipleNamedExports.json");
+        ParseNode? node = analyzer.ReadTokens(output.Tokens);
+        Assert.That(node, Is.Not.Null);
+        if (node is RootNode rootNode)
+        {
+            Assert.That(rootNode.statements.Count, Is.EqualTo(1));
+            var importNode = rootNode.statements[0];
+            Assert.That(importNode, Is.Not.Null);
+        }
+        Assert.That(analyzer.ErrorHandler.ErrorList.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ImportDefaultExport()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Import/ImportDefaultExport.json");
+        ParseNode? node = analyzer.ReadTokens(output.Tokens);
+        Assert.That(node, Is.Not.Null);
+        if (node is RootNode rootNode)
+        {
+            Assert.That(rootNode.statements.Count, Is.EqualTo(1));
+            var importNode = rootNode.statements[0];
+            Assert.That(importNode, Is.Not.Null);
+        }
+        Assert.That(analyzer.ErrorHandler.ErrorList.Count, Is.EqualTo(0));
+    }
+}
