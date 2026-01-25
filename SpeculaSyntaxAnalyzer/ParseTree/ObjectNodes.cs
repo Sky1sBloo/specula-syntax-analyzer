6f92c9e11de5d70bf3bcf68d17519c5fed4ac7e6
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-25 14:32:17 +0800

    Added struct handler

--- SpeculaSyntaxAnalyzer/ParseTree/ObjectNodes.cs
+++ SpeculaSyntaxAnalyzer/ParseTree/ObjectNodes.cs
@@ -2,3 +2,6 @@ namespace SpeculaSyntaxAnalyzer.ParseTree;
 
 public record StructField (string identifier, TypeDefinitionNode typeDef) : ParseNode;
 public record StructDefNode (string structName, PrintableList<StructField> fields) : ParseNode;
+public record InterfaceFuncNode (string funcName, PrintableList<FuncParam> parameters, TypeDefinitionNode returnType) : ParseNode;
+public record InterfaceDefNode (string interfaceName, PrintableList<InterfaceFuncNode> methods) : ParseNode;
+public record ImplDefNode (string structName, PrintableList<FuncDef> methods) : ParseNode;
---
+++ SpeculaSyntaxAnalyzer/SyntaxAnalyzer/Handlers/StructHandler.cs
@@ -0,0 +1,63 @@
+using System.Reflection.Metadata;
+using SpeculaSyntaxAnalyzer.ParseTree;
+
+namespace SpeculaSyntaxAnalyzer.SyntaxAnalyzer;
+
+public class StructHandler: Handler
+{
+    private readonly VarDefinitionHandler typeHandler;
+    public StructHandler(ErrorsHandler errors) : base(errors)
+    {
+        typeHandler = new VarDefinitionHandler(errors);
+    }
+
+    protected ParseNode? verifyTokens()
+    {
+        if (CurrentToken.Type != Token.Types.K_STRUCT)
+        {
+            throw new SyntaxErrorException(["struct"], CurrentToken);
+        }
+        incrementIndex();
+        if (CurrentToken.Type != Token.Types.IDENT)
+        {
+            throw new SyntaxErrorException(["IDENTIFIER"], CurrentToken);
+        }
+        string structName = CurrentToken.Value;
+        incrementIndex();
+        if (CurrentToken.Type != Token.Types.D_CBRAC_OP)
+        {
+            throw new SyntaxErrorException(["{"], CurrentToken);
+        }
+        incrementIndex();
+        PrintableList<StructField> fields = new();
+        while (CurrentToken.Type != Token.Types.D_CBRAC_CLO)
+        {
+            if (CurrentToken.Type != Token.Types.IDENT)
+            {
+                throw new SyntaxErrorException(["IDENTIFIER"], CurrentToken);
+            }
+            string fieldName = CurrentToken.Value;
+            incrementIndex();
+            if (CurrentToken.Type != Token.Types.D_COLON)
+            {
+                throw new SyntaxErrorException([":"], CurrentToken);
+            }
+            incrementIndex();
+            TypeDefinitionNode? fieldType = parseTypeDefinition();
+            if (fieldType == null)
+            {
+                throw new SyntaxErrorException(["TYPE_DEFINITION"], CurrentToken);
+            }
+            fields.Add(new StructField(fieldName, fieldType));
+            if (CurrentToken.Type == Token.Types.COMMA)
+            {
+                incrementIndex();
+            }
+        }
+    }
+
+    private TypeDefinitionNode? parseTypeDefinition()
+    {
+        ParseNode? typeNode = delegateToHandler(typeHandler);
+        return typeNode as TypeDefinitionNode;
+}
