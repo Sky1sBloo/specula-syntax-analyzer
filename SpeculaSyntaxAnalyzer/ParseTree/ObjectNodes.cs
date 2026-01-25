0b7ec17438251e860cec864ac3a24818c75ff998
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-25 16:06:20 +0800

    Added implement handler

--- SpeculaSyntaxAnalyzer/ParseTree/ObjectNodes.cs
+++ SpeculaSyntaxAnalyzer/ParseTree/ObjectNodes.cs
@@ -8,4 +8,5 @@ public interface InterfaceFuncNode : ParseNode;
 public record InterfaceFuncReturnSelfNode(string funcName, PrintableList<ParamNode> parameters) : InterfaceFuncNode;
 public record InterfaceFuncReturnNode (string funcName, PrintableList<ParamNode> parameters, TypeDefinitionNode returnType) : InterfaceFuncNode;
 public record InterfaceDefNode (string interfaceName, PrintableList<InterfaceFuncNode> methods) : RootStatement;
-public record ImplDefNode (string structName, PrintableList<FuncDef> methods) : RootStatement;
+
+public record ImplDefNode (string structName, string interfaceName, PrintableList<FuncDef> methods) : RootStatement;
