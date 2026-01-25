e9ddd9e15533a086cffaaf56f738c911e31511b8
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-25 19:53:25 +0800

    Added parse nodes for contracts

---
+++ SpeculaSyntaxAnalyzer/ParseTree/Contracts/ContractEvents.cs
@@ -0,0 +1,7 @@
+namespace SpeculaSyntaxAnalyzer.ParseTree;
+
+public record ContractEventNodes(RoleNode From, RoleNode To, PrintableList<ContractEventNode> Events) : ParseNode;
+public interface ContractEventNode : ParseNode;
+public record ContractAutoResetEventNode(PrintableList<StateNode> States) : ContractEventNode;
+public record ContractAutoMoveEventNode(PrintableList<StateNode> States, StateNode TargetState) : ContractEventNode;
+public record ContractFailEventNode(string Identifier, PrintableList<ParamNode> Parameters) : ContractEventNode;
