e4f4b096efe50e1c9b1019683488e2479ee46120
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-26 11:30:54 +0800

    Fixed issue with func params handler not escaping on closing token

---
+++ SpeculaSyntaxAnalyzer.Tests/ListenerMessageEventTests.cs
@@ -0,0 +1,128 @@
+using NUnit.Framework;
+using SpeculaSyntaxAnalyzer.LexerReader;
+using SpeculaSyntaxAnalyzer.ParseTree;
+using SpeculaSyntaxAnalyzer.SyntaxAnalyzer;
+
+namespace SpeculaSyntaxAnalyzer.Tests;
+
+[TestFixture]
+public class ListenerMessageEventTests
+{
+    private ListenerMessageEventHandler messageEventHandler = null!;
+    private ErrorsHandler errorsHandler = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        errorsHandler = new();
+        messageEventHandler = new(errorsHandler);
+    }
+
+    [Test]
+    public void BasicCase()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/BasicCase.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        foreach (var err in errorsHandler.ErrorList)
+        {
+            TestContext.WriteLine(err.ToString());
+        }
+        //Assert.That(errorsHandler.ErrorList.Count, Is.EqualTo(0));
+        Assert.That(node.node, Is.TypeOf<ListenerMessageEventNode>());
+        
+        var messageEvent = (ListenerMessageEventNode)node.node!;
+        Assert.That(messageEvent.Name, Is.EqualTo("MoveCommand"));
+        Assert.That(messageEvent.Parameters.Params.Count, Is.EqualTo(2));
+        Assert.That(messageEvent.Parameters.Params[0].Identifier, Is.EqualTo("direction"));
+        Assert.That(messageEvent.Parameters.Params[1].Identifier, Is.EqualTo("speed"));
+    }
+
+    [Test]
+    public void SingleParameter()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/SingleParam.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        Assert.That(errorsHandler.ErrorList.Count, Is.EqualTo(0));
+        Assert.That(node.node, Is.TypeOf<ListenerMessageEventNode>());
+        
+        var messageEvent = (ListenerMessageEventNode)node.node!;
+        Assert.That(messageEvent.Name, Is.EqualTo("SetSpeed"));
+        Assert.That(messageEvent.Parameters.Params.Count, Is.EqualTo(1));
+        Assert.That(messageEvent.Parameters.Params[0].Identifier, Is.EqualTo("value"));
+    }
+
+    [Test]
+    public void NoParameters()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/NoParameters.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        Assert.That(errorsHandler.ErrorList.Count, Is.EqualTo(0));
+        Assert.That(node.node, Is.TypeOf<ListenerMessageEventNode>());
+        
+        var messageEvent = (ListenerMessageEventNode)node.node!;
+        Assert.That(messageEvent.Name, Is.EqualTo("StatusRequest"));
+        Assert.That(messageEvent.Parameters.Params.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void EmptyBody()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/EmptyBody.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        Assert.That(errorsHandler.ErrorList.Count, Is.EqualTo(0));
+        Assert.That(node.node, Is.TypeOf<ListenerMessageEventNode>());
+        
+        var messageEvent = (ListenerMessageEventNode)node.node!;
+        Assert.That(messageEvent.Body.Body.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void NoRespond()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/NoRespond.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        Assert.That(errorsHandler.ErrorList.Count, Is.EqualTo(0));
+        Assert.That(node.node, Is.TypeOf<ListenerMessageEventNode>());
+        
+        var messageEvent = (ListenerMessageEventNode)node.node!;
+        Assert.That(messageEvent.Name, Is.EqualTo("LogCommand"));
+        // Body should have statements but no respond
+        Assert.That(messageEvent.Body.Body.Count, Is.GreaterThan(0));
+    }
+
+    [Test]
+    public void InvalidMissingName()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/Invalid/NoName.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        // Should have errors
+        Assert.That(errorsHandler.ErrorList.Count, Is.GreaterThan(0));
+    }
+
+    [Test]
+    public void InvalidMissingBodyBraces()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/Invalid/NoBodyBraces.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        // Should have errors
+        Assert.That(errorsHandler.ErrorList.Count, Is.GreaterThan(0));
+    }
+
+    [Test]
+    public void InvalidMissingParameterBraces()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/Invalid/NoParamBraces.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        // Should have errors
+        Assert.That(errorsHandler.ErrorList.Count, Is.GreaterThan(0));
+    }
+
+    [Test]
+    public void InvalidTrailingComma()
+    {
+        var output = LexerFileReader.ParseFile("Samples/Listener/MessageEvents/Invalid/TrailingComma.json");
+        HandlerOutput node = messageEventHandler.HandleToken(output.Tokens, 0);
+        // Should have errors
+        Assert.That(errorsHandler.ErrorList.Count, Is.GreaterThan(0));
+    }
+}
