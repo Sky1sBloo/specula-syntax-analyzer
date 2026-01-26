95672f7b813f16e1b9a639a6e66f4ee8df14ba4a
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-26 14:39:29 +0800

    Added capability nd spawn thread node

--- SpeculaSyntaxAnalyzer/ParseTree/Capabilities.cs
+++ SpeculaSyntaxAnalyzer/ParseTree/Capabilities.cs
@@ -17,3 +17,7 @@ public enum CapabilityTypes
 public record Capabilities(PrintableList<Capability> CapabilityList) : ParseNode;
 
 public record Capability(CapabilityTypes Type, PrintableList<string> Configuration) : ParseNode;
+public record MoveValueNode(Movable Value) : Expression, Movable;
+public record ShareValueNode(Movable Value) : Expression, Movable;
+public record RefValueNode(Movable Value) : Expression, Movable;
+public record ViewValueNode(Movable Value) : Expression, Movable;
