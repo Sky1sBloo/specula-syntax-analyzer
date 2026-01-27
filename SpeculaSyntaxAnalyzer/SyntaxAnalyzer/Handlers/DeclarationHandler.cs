ddd269dbadcc39c56340c7cca1623d66d5f73bf1
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-27 09:22:42 +0800

    Made identifier infer

--- SpeculaSyntaxAnalyzer/SyntaxAnalyzer/Handlers/DeclarationHandler.cs
+++ SpeculaSyntaxAnalyzer/SyntaxAnalyzer/Handlers/DeclarationHandler.cs
@@ -85,10 +85,10 @@ public class DeclarationHandler : Handler
 
         if (expr is IdentifierValue)
         {
-            return new TypeNode(DataTypes.IDENTIFIER);
+            return new TypeNode(DataTypes.INFER);
         }
 
-        return new TypeNode(DataTypes.UNKNOWN);
+        return new TypeNode(DataTypes.INFER);
     }
 
     private DeclarationStatementNode ConstructNode()
